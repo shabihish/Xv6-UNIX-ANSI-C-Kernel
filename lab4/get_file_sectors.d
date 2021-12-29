d7b4e95c6bc3ef71f3ac14d5cfae2dba72f799d5
Amirmahdi Ansaripour <ansaripouramirmahdi@gmail.com>
2021-12-29 19:02:54 +0000

    Delete based on incomplete upload

--- lab4/get_file_sectors.d
+++
@@ -1,2 +0,0 @@
-get_file_sectors.o: get_file_sectors.c /usr/include/stdc-predef.h types.h \
- stat.h user.h
--- lab4/test_get_parent_pid.d
+++
@@ -1,2 +0,0 @@
-test_get_parent_pid.o: test_get_parent_pid.c /usr/include/stdc-predef.h \
- types.h stat.h user.h
